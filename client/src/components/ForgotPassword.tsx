db1909409d6239419b8b630da563251745437052
elcalzado <elcalzado@me.com>
2025-11-12 18:35:19 -0500

    Dependencies and frontend env

--- client/src/components/ForgotPassword.tsx
+++ client/src/components/ForgotPassword.tsx
@@ -4,44 +4,68 @@ import axios, { AxiosError } from "axios";
 import "./PasswordReset.css";
 import TextField from "@mui/material/TextField";
 const ForgotPassword = () => {
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post(
-        "http://localhost:5050/api/user/request-password-reset",
-        { email }
-      );
-      setMessage(res.data.message);
-    } catch (err) {
-      const error = err as AxiosError<{ message?: string }>;
-      setMessage(error.response?.data?.message || "Something went wrong");
-    }
-  };
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post(
+				`${API_BASE_URL}/api/user/request-password-reset`,
+				{ email }
+			);
+			setMessage(res.data.message);
+		} catch (err) {
+			const error = err as AxiosError<{ message?: string }>;
+			setMessage(error.response?.data?.message || "Something went wrong");
+		}
+	};
 
-  return (
-    <div className="auth-container">
-      <div className="content">
-        <h2>Forgot Password</h2>
-        <form onSubmit={handleSubmit} style ={{display:"flex", gap:"10px", flexDirection:"column",width:"100%", alignItems:"center"}}>
-          <TextField
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            sx={{width:"70%"}}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
-            required
-          />
-          <button type="submit" style={{width:"30%", minWidth:"200px"}}>Send Reset Link</button>
-        </form>
-        {message && <p style={{fontSize: "20px", fontWeight: "500", color:"red"}}>{message}</p>}
-      </div>
-    </div>
-  );
+	return (
+		<div className="auth-container">
+			<div className="content">
+				<h2>Forgot Password</h2>
+				<form
+					onSubmit={handleSubmit}
+					style={{
+						display: "flex",
+						gap: "10px",
+						flexDirection: "column",
+						width: "100%",
+						alignItems: "center",
+					}}
+				>
+					<TextField
+						type="email"
+						placeholder="Enter your email"
+						value={email}
+						sx={{ width: "70%" }}
+						onChange={(e: ChangeEvent<HTMLInputElement>) =>
+							setEmail(e.target.value)
+						}
+						required
+					/>
+					<button
+						type="submit"
+						style={{ width: "30%", minWidth: "200px" }}
+					>
+						Send Reset Link
+					</button>
+				</form>
+				{message && (
+					<p
+						style={{
+							fontSize: "20px",
+							fontWeight: "500",
+							color: "red",
+						}}
+					>
+						{message}
+					</p>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default ForgotPassword;
--- client/src/components/ResetPassword.tsx
+++ client/src/components/ResetPassword.tsx
@@ -5,49 +5,75 @@ import axios, { AxiosError } from "axios";
 import "./PasswordReset.css";
 import TextField from "@mui/material/TextField";
 const ResetPassword = () => {
-  const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
-  const token = searchParams.get("token");
+	const [searchParams] = useSearchParams();
+	const navigate = useNavigate();
+	const token = searchParams.get("token");
 
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+	const [password, setPassword] = useState("");
+	const [message, setMessage] = useState("");
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post(
-        "http://localhost:5050/api/user/reset-password",
-        { token, password }
-      );
-      setMessage(res.data.message);
-      setTimeout(() => navigate("/"), 2000);
-    } catch (err) {
-      const error = err as AxiosError<{ message?: string }>;
-      setMessage(error.response?.data?.message || "Error resetting password");
-    }
-  };
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post(
+				`${API_BASE_URL}/api/user/reset-password`,
+				{ token, password }
+			);
+			setMessage(res.data.message);
+			setTimeout(() => navigate("/"), 2000);
+		} catch (err) {
+			const error = err as AxiosError<{ message?: string }>;
+			setMessage(
+				error.response?.data?.message || "Error resetting password"
+			);
+		}
+	};
 
-  return (
-    <div className="auth-container">
-      <div className="content">
-        <h2>Reset Password</h2>
-        <form onSubmit={handleSubmit}  style ={{display:"flex", gap:"10px", flexDirection:"column",width:"100%", alignItems:"center",}}>
-          <TextField
-            type="password"
-            placeholder="Enter new password"
-            value={password}
-            sx={{width:"70%"}}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setPassword(e.target.value)
-            }
-            required
-          />
-          <button type="submit" style={{width:"30%", minWidth:"200px"}}>Reset Password</button>
-        </form>
-        {message && <p style={{fontSize: "20px", fontWeight: "500", color:"red"}}>{message}</p>}
-      </div>
-    </div>
-  );
+	return (
+		<div className="auth-container">
+			<div className="content">
+				<h2>Reset Password</h2>
+				<form
+					onSubmit={handleSubmit}
+					style={{
+						display: "flex",
+						gap: "10px",
+						flexDirection: "column",
+						width: "100%",
+						alignItems: "center",
+					}}
+				>
+					<TextField
+						type="password"
+						placeholder="Enter new password"
+						value={password}
+						sx={{ width: "70%" }}
+						onChange={(e: ChangeEvent<HTMLInputElement>) =>
+							setPassword(e.target.value)
+						}
+						required
+					/>
+					<button
+						type="submit"
+						style={{ width: "30%", minWidth: "200px" }}
+					>
+						Reset Password
+					</button>
+				</form>
+				{message && (
+					<p
+						style={{
+							fontSize: "20px",
+							fontWeight: "500",
+							color: "red",
+						}}
+					>
+						{message}
+					</p>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default ResetPassword;
