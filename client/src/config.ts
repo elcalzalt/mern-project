cea26448f35bfe602f0af4a488d2e19e90ac849c
elcalzado <elcalzado@me.com>
2025-11-12 19:46:08 -0500

    Removed .env

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-const FALLBACK_API_BASE_URL = "http://localhost:5050";
+const FALLBACK_API_BASE_URL = "https://matlikeys.com";
 
 export const API_BASE_URL =
 	import.meta.env.VITE_API_BASE_URL || FALLBACK_API_BASE_URL;
